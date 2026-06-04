476b22eb9e373687093bbf8e1a415e1bd4b8b946
Jean <154575672+Veuxirdev@users.noreply.github.com>
2026-06-04 14:47:04 -0400

    Add hash function as method of HashMap class

--- hash-map.js
+++ hash-map.js
@@ -4,15 +4,16 @@ class HashMap {
     this.loadFactor = 0.75;
     this.capacity = 16;
   }
-}
 
-function hash(key) {
-  let hashCode = 0;
+  hash(key) {
+    let hashCode = 0;
 
-  const primeNumber = 31;
-  for (let i = 0; i < key.length; i++) {
-    hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode %= 16;
+    }
+
+    return hashCode;
   }
-
-  return hashCode;
 }
