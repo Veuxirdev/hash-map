8fda532e77f2cca86ba1d35e818da6aef958608b
Jean <154575672+Veuxirdev@users.noreply.github.com>
2026-06-04 14:40:32 -0400

    Add hash function

--- hash-map.js
+++ hash-map.js
@@ -5,3 +5,14 @@ class HashMap {
     this.capacity = 16;
   }
 }
+
+function hash(key) {
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < key.length; i++) {
+    hashCode = primeNumber * hashCode + key.charCodeAt(i);
+  }
+
+  return hashCode;
+}
